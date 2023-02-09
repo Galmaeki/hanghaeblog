1ca258a6549e95b48b8910349f7671537def0bf5
Galmaeki <kjh09800@gmail.com>
2023-02-09 09:59:20 +0900

    출력내용 수정시간 id 업데이트

--- src/main/java/com/example/hanghaeblog/dto/PostDto.java
+++ src/main/java/com/example/hanghaeblog/dto/PostDto.java
@@ -3,6 +3,8 @@ package com.example.hanghaeblog.dto;
 import com.example.hanghaeblog.entity.Posts;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostDto {
     private String title;
@@ -10,9 +12,14 @@ public class PostDto {
     private String content;
 
     private String author;
+    private LocalDateTime modifiedAt;
+
+    private Long id;
     public PostDto(Posts post){
         this.author=post.getAuthor();
         this.title=post.getTitle();
         this.content=post.getContent();
+        this.id=post.getId();
+        this.modifiedAt=post.getModifiedAt();
     }
 }
