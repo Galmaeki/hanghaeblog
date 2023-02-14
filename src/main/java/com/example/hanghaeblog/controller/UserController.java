6b3f59d01c91d5bb2716127c2c0d38ee129922c9
Galmaeki <kjh09800@gmail.com>
2023-02-14 14:35:16 +0900

    로그인 구현

--- src/main/java/com/example/hanghaeblog/controller/UserController.java
+++ src/main/java/com/example/hanghaeblog/controller/UserController.java
@@ -6,8 +6,10 @@ import com.example.hanghaeblog.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
@@ -24,8 +26,20 @@ public class UserController {
     else{
         post.setSucess("실패");
     }
-
     return post;
+    }
+
+    @ResponseBody
+    @PostMapping("/login")
+    public ResponseDto join(@RequestBody UsersRequestDto usersRequestDto, HttpServletResponse response){
+        ResponseDto<String> post = new ResponseDto();
+        post.setData(usersService.login(usersRequestDto,response));
+        if(post.getData().equals("성공"))
+            post.setSucess("성공");
+        else{
+            post.setSucess("실패");
+        }
+        return post;
     }
 
 }
--- src/main/java/com/example/hanghaeblog/service/UsersService.java
+++ src/main/java/com/example/hanghaeblog/service/UsersService.java
@@ -2,21 +2,21 @@ package com.example.hanghaeblog.service;
 
 import com.example.hanghaeblog.dto.UsersRequestDto;
 import com.example.hanghaeblog.entity.Users;
+import com.example.hanghaeblog.jwt.JwtUtil;
 import com.example.hanghaeblog.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UsersService {
     private final UsersRepository usersRepository;
+    private final JwtUtil jwtUtil;
 
-    private void jungbok(String username) {
-
-    }
 
     @Transactional
     public String join(UsersRequestDto usersRequestDto) {
@@ -32,5 +32,29 @@ public class UsersService {
         catch(IllegalArgumentException e){
             return "중복된 이름입니다.";
         }
+    }
+
+    @Transactional(readOnly = true)
+    public String login(UsersRequestDto usersRequestDto, HttpServletResponse response) {
+        String name = usersRequestDto.getUsername();
+        String password = usersRequestDto.getPassword();
+        try{
+        Users user = usersRepository.findByUsername(name).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+        try{
+            if(!user.getPassword().equals(password)){
+                throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+            }
+            response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername()));
+            return "성공";
+        }catch (IllegalArgumentException e){
+            return "비밀번호를 확인해 주세요";
+        }
+        }catch(IllegalArgumentException e){
+            return "등록된 사용자가 아닙니다";
+        }
+
+
     }
 }
